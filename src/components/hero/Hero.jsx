0dc20bb6a7f926f1e5506d5beb83aab679c93bcf
Ardy1an <adithardiyanto0@gmail.com>
2025-03-16 17:21:54 +0700

    responsive hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./hero.css";
 import sofa from "../../assets/sofa.png";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 
 const Hero = () => {
   return (
@@ -10,23 +11,24 @@ const Hero = () => {
       </div>
 
       <div className="text-hero">
-        <span className="headline">
-          We Create your home <br />
-          More aesthetic
-        </span>
+        <span className="headline-hero">We Create Your Home More Aesthetic</span>
         <span className="text1">Furniture power is a software as a service for multipurpose business management systems.</span>
         <div className="checklist">
           <div className="checklist-item">
-            <span className="check-icon">✔</span>
+            <span className="check-icon">
+              <IoIosCheckmarkCircle />
+            </span>
             <div className="test">
               <span className="check-title">Valuation Services</span>
               <div className="check-description">Sometimes features require a short description. This can be detailed description</div>
             </div>
           </div>
           <div className="checklist-item">
-            <span className="check-icon">✔</span>
+            <span className="check-icon">
+              <IoIosCheckmarkCircle />
+            </span>
             <div>
-              <span className="check-title">Valuation Services</span>
+              <span className="check-title">Development of Furniture Modelss</span>
               <div className="check-description">Sometimes features require a short description. This can be detailed description</div>
             </div>
           </div>
